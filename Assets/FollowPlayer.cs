cebfee3f351e698304f6a89965ab9cb6be9f3db2
ceci <144265428+ceci11pc@users.noreply.github.com>
2023-09-09 14:04:25 -0300

    first commit

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    //para poder tener la posicion del auto
+    public GameObject player;
+    // para que la camara este arriba y no justo en el medio del auto
+    private Vector3 offset = new Vector3(0, 0, 2.65f);
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
