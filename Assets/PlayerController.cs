af9538b49f40d217245fb964d8d8dd2168c95a74
ceci <144265428+ceci11pc@users.noreply.github.com>
2023-09-20 13:46:26 -0300

    Player Controller done

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed;
+    
+    private float turnSpeed = 5.0f;
+    private float horizontalInput;
+    float fowardInput; // creo que es el load 0 o 1 al pulsar 
+    float speed;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +20,27 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed , Space.World);
+        horizontalInput = Input.GetAxis("Horizontal");
+        fowardInput = Input.GetAxis("Vertical");
+        
+        Debug.Log("speed " + speed);
+
+      
+        if(fowardInput > 0)
+        {
+            speed += 0.1f;
+        }
+        else if (fowardInput <= 0 && speed >= 0.5f) 
+        {
+            speed -= 0.5f;
+        }
+        else
+        {
+            speed = 0;
+        }
+        
+
+        transform.Translate(speed * Time.deltaTime * Vector3.forward , Space.World);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
