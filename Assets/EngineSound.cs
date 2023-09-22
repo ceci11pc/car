3cefce07d5170a34baa226fedebc27e143ef6904
cecigarciaf <cecigarciaf@gmail.com>
2023-09-22 11:37:57 -0300

    Engine & Tyres sounds done.
    Also modified Player Controller script

--- Assets/EngineSound.cs
+++ Assets/EngineSound.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class EngineSound : MonoBehaviour
 {
+
+    FMOD.Studio.EventInstance engine;
+    FMOD.Studio.EventInstance tyres;
+    FMOD.Studio.PARAMETER_ID rpmId, loadId;
+
+    PlayerController Car;
+
+    void Awake()
+    {
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        engine = FMODUnity.RuntimeManager.CreateInstance("event:/ENGINE");
+        tyres = FMODUnity.RuntimeManager.CreateInstance("event:/TYRES");
+
+        FMOD.Studio.EventDescription engineEventDescription;
+        engine.getDescription(out engineEventDescription);
+
+        FMOD.Studio.PARAMETER_DESCRIPTION engineRpmParameterDescription;
+        engineEventDescription.getParameterDescriptionByName("RPM", out engineRpmParameterDescription);
+
+        rpmId = engineRpmParameterDescription.id;
+
+        FMOD.Studio.PARAMETER_DESCRIPTION engineLoadParameterDescription;
+        engineEventDescription.getParameterDescriptionByName("Accel", out engineLoadParameterDescription);
+
+        loadId = engineLoadParameterDescription.id;
         
+
+        Car = GetComponent<PlayerController>();
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(tyres, GetComponent<Transform>(), GetComponent<Rigidbody>());
+        engine.start();
+        tyres.start();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+        engine.setParameterByID(rpmId, Car.speed);
+        engine.setParameterByID(loadId, Car.fowardInput);
+        tyres.setParameterByID(rpmId, Car.speed);
+
     }
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -8,8 +8,10 @@ public class PlayerController : MonoBehaviour
     
     private float turnSpeed = 5.0f;
     private float horizontalInput;
-    float fowardInput; // creo que es el load 0 o 1 al pulsar 
-    float speed;
+    public float revs;
+    public float fowardInput; // creo que es el load 0 o 1 al pulsar 
+    public float speed;
+    //public float load;
 
     // Start is called before the first frame update
     void Start()
@@ -22,25 +24,47 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         fowardInput = Input.GetAxis("Vertical");
-        
-        Debug.Log("speed " + speed);
+        //load = fowardInput > 0 ? 1 : 0;
 
-      
-        if(fowardInput > 0)
+
+        //si esta acelerando y la speed no llegó al max, y revs no llegó al max sube la speed y revs
+        if(fowardInput > 0 && speed < 50 && revs < 1)
         {
             speed += 0.1f;
+            revs += 0.005f;
         }
+
+        //si esta acelerando y la speed no llegó al max, y revs sí llegó al max baja la speed y revs
+        else if (fowardInput > 0 && speed < 50 && revs >= 1)
+        {
+            speed -= 2f;
+            revs = 0.6f;
+        }
+
+        //si esta acelerando y la speed llegó al max, se mantiene
+        else if (fowardInput > 0 && speed >= 50)
+        {
+            speed = 50;
+        }
+        //si no está acelerando y no está frenado, bajá la speed
         else if (fowardInput <= 0 && speed >= 0.5f) 
         {
             speed -= 0.5f;
+            revs -= 0.1f;
         }
-        else
+        //sino, la speed es 0
+         else
         {
-            speed = 0;
+        speed = 0f;
+         revs = 0;
         }
-        
 
-        transform.Translate(speed * Time.deltaTime * Vector3.forward , Space.World);
+
+
+
+
+
+    transform.Translate(speed * Time.deltaTime * Vector3.forward , Space.World);
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
